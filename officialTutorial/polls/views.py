0a875ae442c6e14364aa83ee21fee5dbff78b6f5
mokhs00 <mokhs00@naver.com>
2021-08-16 14:20:59 +0900

    add : official tutorial step 4

--- officialTutorial/polls/views.py
+++ officialTutorial/polls/views.py
@@ -5,6 +5,7 @@ from django.db.models import F
 from django.views import generic
 from .models import *
 
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -13,13 +14,16 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+
 class ResultView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
 
