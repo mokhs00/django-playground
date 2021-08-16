f152a2a2a129662055e070474a431c12899dae0d
mokhs00 <mokhs00@naver.com>
2021-08-16 14:10:59 +0900

    snapshot : 제너릭 뷰 사용하기 이전 snapshot

--- officialTutorial/polls/views.py
+++ officialTutorial/polls/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.db.models import F
 from .models import *
 
 
@@ -16,13 +18,28 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
-    return render(request, 'polls/detail.html', {'question' : question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def result(request, question_id):
-    response = "You're looking at results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
