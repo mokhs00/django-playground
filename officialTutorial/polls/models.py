b4be2b86a1b30c8650d487c04965194152ac7546
mokhs00 <mokhs00@naver.com>
2021-08-16 15:46:22 +0900

    add : official tutorial step 7 admin 페이지 커스텀하기

--- officialTutorial/polls/models.py
+++ officialTutorial/polls/models.py
@@ -1,4 +1,5 @@
 import datetime
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -6,9 +7,19 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    # 3.1 버전엔 없어요,, 다르게 해야함..
+    # @admin.display(
+    #     boolean=True,
+    #     ordering='pub_date',
+    #     description='Published recently?',
+    # )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.question_text
